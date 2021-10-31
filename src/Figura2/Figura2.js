e519e881ccc899f60de0243f3d4f155677ab5fbc
datu1210 <turizod@gmail.com>
2021-10-30 19:17:14 -0500

    trabajando un rato mas

--- src/Figura2/Figura2.js
+++ src/Figura2/Figura2.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Figura2.css'
 
 const Figura2 = () => {
     return (
-        <div>
-            <h1>Esta es la Figura 2</h1>
+        <div className="container">
+            <div id="circulo">
+                <div id="centro">
+                    <div id="hueco">
+                        <div id="ends"></div>
+                        <div id="ends2"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
